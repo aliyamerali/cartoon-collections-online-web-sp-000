66b9bb5f6069f8f083046ca1341835a4f1942ec9
Aliya Merali <aliya.merali@flatironschool.com>
2020-05-28 03:08:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,8 @@ end
 # planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 # print summon_captain_planet(planeteer_calls)
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? {|call| call.length > 4}
 end
 
 def find_the_cheese# code an argument here
