96649f873152b05bf27d43b45896c2e7aaa4f967
Aliya Merali <aliya.merali@flatironschool.com>
2020-05-28 03:04:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 # dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 # roll_call_dwarves(dwarves)
 
-def summon_captain_planet(planeteer_calls) 
+def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |call| call.capitalize << "!"}
 end
 
